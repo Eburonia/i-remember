5eefad227055b5060d7452ca39018acf7f555c70
Eburonia <maurice.vossen@planet.nl>
2021-07-20 09:29:45 +0000

    feat: add navbar toggle functionality to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+/* hides toggle menu at loading website */
+$('#toggle-menu').toggle();
+
+
+/* toggle menu */
+$('#navbar-toggle-button').on('click', function() {
+
+    $('#toggle-menu').slideToggle('slow');
+
+});
+
+        
