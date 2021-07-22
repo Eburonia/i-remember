f3865ea79f5b2276999a1fe5961ed0761f701602
Eburonia <maurice.vossen@planet.nl>
2021-07-22 08:18:18 +0000

    script: add functionality for cards summary in decks.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,12 @@ function loadDecksOnScreen() {
     // empty the my-decks div
     $('#my-decks').empty();
 
+    // add title bar
     $('#my-decks').append(`
     
     <div class="deck-field deck-title">my decks</div>
     
     `);
-
-
 
     for(let i = 0; i < 10; i++) {
 
@@ -25,16 +24,97 @@ function loadDecksOnScreen() {
             <div class="deck-field deck-edit">edit</div>
 
         `);
-    
     }
+}
 
+
+
+
+function editDeck(deckNumber) {
+
+    // empty the my-decks div
+    $('#my-decks').empty();
+
+
+
+    // if deck does not exist, create empty deck on screen
+    if(localStorage.getItem('deck' + deckNumber) == null) {
+
+        // add title bar 
+        $('#my-decks').append(`
+    
+        <div class="cards-field deck-title">my deck1</div>
+        <div class="cards-field cards-title">English</div>
+        <div class="cards-field cards-title">Dutch</div>
+
+        `);
+
+
+        $('#my-decks').append(`
+
+            <div class="cards-field cards-front">empty</div>
+            <div class="cards-field cards-back">empty</div>
+    
+    `);
+
+        // if deck does exist, load from localstorage
+        }
+        else {
+            alert('not-empty');
+        }
+
+        // add buttons 
+        $('#buttons-div').append(`
+
+            <button id="add-card-btn">add card</button>
+
+        `);
+
+
+
+     
+
+
+    
 
 }
 
-$(document).on('click', '.deck-descr', function() {
+// add card button
+$(document).on('click', '#add-card-btn', function(){
 
-    
+    $('#my-decks').append(`
+
+    <div class="cards-field cards-front">empty</div>
+    <div class="cards-field cards-back">empty</div>
+
+`);
 
 });
 
 
+
+$(document).on('click', '.deck-edit', function() {
+
+    editDeck($('.deck-edit').index(this));
+
+});
+
+
+
+
+
+
+
+
+
+
+/* hides toggle menu at loading website */
+$('#toggle-menu').toggle();
+
+
+/* toggle menu */
+$('#navbar-toggle-button').on('click', function() {
+
+    $('#toggle-menu').slideToggle('slow');
+
+}); 
