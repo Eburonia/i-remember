7ea89c14e7f8d67fc3fb1feacaab27ada8470d24
Eburonia <maurice.vossen@planet.nl>
2021-08-03 11:08:34 +0000

    feat: add deck load function to editor

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,15 +1,76 @@
 
-
 $('#content-editor-page').append(`
 
     <div id="cards-summary-div">
     
-        <div id="cards-summary-title">Deck Name</div>
-    
-        <div class="cards-summary-frontside">front</div>
-        <div class="cards-summary-backside">back</div>
+        
     
     </div>
 
+    <div id="cards-summary-buttons">
+        
+            <input type="file" id="browse-button" />
+            <button id="read-button">Read File</button>
+            <button id="add-row">Add Row</button>
+
+    </div>
 
 `);
+
+
+
+
+let deck = 0;
+let numberOfCardsInDeck;
+
+document.querySelector("#read-button").addEventListener('click', function() {
+
+		let file = document.querySelector("#browse-button").files[0];
+		let reader = new FileReader();
+
+		reader.addEventListener('load', function(e) {
+
+	    	let text = e.target.result;
+            deck = text;
+            deck = JSON.parse(deck);
+            numberOfCardsInDeck = deck.length;
+            
+            $('#cards-summary-div').empty();
+            
+            $('#cards-summary-div').append(`<div id="cards-summary-title">Deck Name</div>`);
+
+            
+
+
+            for(let i = 0; i < numberOfCardsInDeck; i++) {
+
+                $('#cards-summary-title').after(`
+            
+                <div class="cards-summary-frontside">${deck[i].brand}</div>
+                <div class="cards-summary-backside">${deck[i].color}</div>
+            
+                `);
+
+            }
+
+		});
+
+		reader.readAsText(file);
+        
+});
+
+
+
+$('#add-row').on('click', function(){
+
+    
+
+
+    $('#cards-summary-title').after(`
+    
+    <div class="cards-summary-frontside">-</div>
+    <div class="cards-summary-backside">-</div>
+    
+    `);
+
+});
