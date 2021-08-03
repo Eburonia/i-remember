75e85f0c815c61930bf8e9d41e79b529143836b0
Eburonia <maurice.vossen@planet.nl>
2021-08-03 10:06:49 +0000

    feat: create editor page

---
+++ assets/js/editor.js
@@ -0,0 +1,15 @@
+
+
+$('#content-editor-page').append(`
+
+    <div id="cards-summary-div">
+    
+        <div id="cards-summary-title">Deck Name</div>
+    
+        <div class="cards-summary-frontside">front</div>
+        <div class="cards-summary-backside">back</div>
+    
+    </div>
+
+
+`);
